b418ac38ecbea3abca3ae9b04bea50596a30dd49
JillKlatt <klatt.jill@gmail.com>
2021-02-02 19:16:54 -0600

    commit tree 1c4424614cb9111b5d87fcbfa60205da8f556c1e

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,35 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  myarray = ["element1", "element2", "element3", "element4"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(myarray, element)
+  myarray << "arrays!"
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def add_element_to_start_of_array(myarray, element)
+  myarray.unshift("wow")
 end
 
-def remove_element_from_end_of_array(array)
-  
+def remove_element_from_end_of_array(myarray)
+  myarray.pop
 end
 
-def remove_element_from_start_of_array(array)
-  
+def remove_element_from_start_of_array(myarray)
+  myarray.shift
 end
 
-def retrieve_element_from_index(array, index_number)
-  
+def retrieve_element_from_index(myarray, index_number)
+  myarray[2]
 end
 
-def retrieve_first_element_from_array(array)
-  
+def retrieve_first_element_from_array(myarray)
+  myarray[0]
 end
 
-def retrieve_last_element_from_array(array)
-  
+def retrieve_last_element_from_array(myarray)
+  myarray[-1]
 end
